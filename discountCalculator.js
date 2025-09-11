0a8d06621efb88d8b4609475deabcf2b604515e8
Katherine <kaburbank@gmail.com>
2025-09-10 18:13:10 -0600

    Your commit message

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,10 +1,9 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+    debugger  
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
     }
-
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
